07b90b2314ffc7ab78f3c9db687098d854bf5298
MaksShadows <Lykanenko1995@gmail.com>
2019-12-28 14:13:40 +0200

    32323

--- src/script/showWeek.js
+++ src/script/showWeek.js
@@ -1,21 +1,20 @@
  function showWeek () {
 
    const weeks = document.querySelector('.days');
-  const arrElems =[];
     const arrWeeks =  ["Mon", "Tue", "Wen", "Tuh", "Fri", "Sat", "Sun"];
 
 
    for (let i = 0; i <= 7; i++) {
       const week = document.createElement('div');
          week.classList.add('day'); 
-         const weekDays = document.createElement('span');
-         weekDays.classList.add()
-
+        
         arrWeeks.push(week);
          weeks.append(week);
      };
-        arrElems.push('day')
+     arrWeeks.push('day')
       weeks.append(...arrWeeks);
   };
 
  showWeek();
+
+ export { showWeek };

54704191d8f38b6a235face5fb0d87594e774b9b
MaksShadows <Lykanenko1995@gmail.com>
2019-12-29 18:31:29 +0200

    add

--- src/script/showWeek.js
+++ src/script/showWeek.js
@@ -5,19 +5,19 @@ function showWeek() {
     const arrElems = [];
     const arrWeeks = ["Mon", "Tue", "Wen", "Tuh", "Fri", "Sat", "Sun"];
     const headerWeek = document.querySelector('.week');
-    let headerWeekHTML = '';
+    let WeekHTML = '';
 
     for (let i = 0; i < 7; i++) {
         const day = document.createElement('div');
 
-        headerWeekHTML +=
+        WeekHTML +=
             `<div class="day">
                 <span class="day_nameDay">${arrWeeks[i]}</span>
             </div>`;
         day.classList.add('column-day');
         arrElems.push(day);
     }
-    headerWeek.innerHTML = headerWeekHTML;
+    headerWeek.innerHTML = WeekHTML;
     days.append(...arrElems);
 };
 

6f09b071ed8e341afcfec8b11bb3b31affbe7194
MaksShadows <Lykanenko1995@gmail.com>
2020-01-02 20:01:09 +0200

    121

--- src/script/showWeek.js
+++ src/script/showWeek.js
@@ -1,42 +1,35 @@
 
+
  function showWeek() {
-     const days = document.querySelector('.days');
-     days.innerHTML = '';
-     const arrElems = [];
-     const arrWeeks = ["Mon", "Tue", "Wen", "Tuh", "Fri", "Sat", "Sun"];
-     const headerWeek = document.querySelector('.week');
-     let WeekHTML = '';
-     let weekday = getShowedMonday();
+    const days = document.querySelector('.days');
+    days.innerHTML = '';
+    const arrElems = [];
+    const arrWeeks = ["Mon", "Tue", "Wen", "Tuh", "Fri", "Sat", "Sun"];
+    const arrDays = ["1", "2", "3", "4", "5", "6", "7"];
+    const headerWeek = document.querySelector('.week');
+    let WeekHTML = '';
 
 
+        for (let i = 0; i < 7; i++) {
+        const week = document.createElement('div');
+       
+        WeekHTML +=
+            `<div class="day">
+                <span class="day_nameDay">${arrWeeks[i]}</span>
+                <div class="day_numberDay">${arrDays[i]}</div>
+                </div>`;
+            week.classList.add('column-day');
+         
+            arrElems.push(week);
 
-         for (let i = 0; i < 7; i++) {
-         const week = document.createElement('div');
-           const valueDate = weekday.getDate() < 10 ? '0' + weekday.getDate() : weekday.getDate();
-
-         WeekHTML +=
-             `<div class="day">
-                 <span class="day_nameDay">${arrWeeks[i]}</span>
-                 <div class="day_numberDay">${valueDate}</div>
-            </div>`;
-             week.classList.add('column-day');
-             arrElems.push(week);
-
-         };
-     headerWeek.innerHTML = WeekHTML;
-     days.append(...arrElems);
- };
+        };
+    headerWeek.innerHTML = WeekHTML;
+    days.append(...arrElems);
+};
 
 export { showWeek };
 
 
-function getShowedMonday() {
-    if (getItemStorage('showedMonday')) {
-        return new Date(getItemStorage('showedMonday'));
-    }
-};
 
-function getItemStorage(key) {
-    return JSON.parse(localStorage.getItem(key));
-};
+
 

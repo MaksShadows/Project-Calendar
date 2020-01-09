61d1c14cf77d20b12b5a3127d8b320d70780acbf
MaksShadows <Lykanenko1995@gmail.com>
2020-01-09 20:13:33 +0200

    add

--- src/script/showWeek.js
+++ src/script/showWeek.js
@@ -4,28 +4,52 @@
     const days = document.querySelector('.days');
     days.innerHTML = '';
     const arrElems = [];
-    const arrWeeks = ["Mon", "Tue", "Wen", "Tuh", "Fri", "Sat", "Sun"];
-    const arrDays = ["1", "2", "3", "4", "5", "6", "7"];
     const headerWeek = document.querySelector('.week');
     let WeekHTML = '';
 
 
+
         for (let i = 0; i < 7; i++) {
         const week = document.createElement('div');
-       
+        let Weeks = getCurWeekDates();
+      
         WeekHTML +=
             `<div class="day">
-                <span class="day_nameDay">${arrWeeks[i]}</span>
-                <div class="day_numberDay">${arrDays[i]}</div>
+                <span class="day_nameDay">${Weeks[i]}</span>
+                <div class="LittleBorder"></div>
                 </div>`;
             week.classList.add('column-day');
-         
             arrElems.push(week);
-
+          
         };
     headerWeek.innerHTML = WeekHTML;
     days.append(...arrElems);
+    
 };
+
+ export function getCurWeekDates() {
+    let monday = getMonday();
+  
+    let arr = [];
+    arr.push(monday);
+  
+    for(let i = 1; i < 7; i++){
+        let nextDay = new Date();
+        nextDay.setDate(monday.getDate() + i);
+        arr.push(nextDay);
+    }
+  
+    console.log(arr);
+  
+    return arr;   
+  }
+  
+   export function getMonday() {
+    let cur = new Date();
+    let day = cur.getDay(),
+        diff = cur.getDate() - day + (day == 0 ? -6 : 1); // adjust when day is sunday
+    return new Date(cur.setDate(diff));
+  }
 
 export { showWeek };
 

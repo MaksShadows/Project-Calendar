11119ad65d072169cc73721b453b18c09984ae03
MaksShadows <Lykanenko1995@gmail.com>
2020-01-13 13:56:12 +0200

    add

--- src/script/switchingWeeks.js
+++ src/script/switchingWeeks.js
@@ -1,5 +1,5 @@
-export   const arrWeeks = ["Mon", "Tue", "Wen", "Tuh", "Fri", "Sat", "Sun"];
 
+import { showWeek } from './showWeek.js';
 
 
 function switchWeeks() {
@@ -8,25 +8,24 @@ function switchWeeks() {
     const arowsLeft = document.querySelector('.navigate__arows_left');
     arowsLeft.addEventListener('click', moveWeek);
     const buttonToday = document.querySelector('.navigate_today');
-    buttonToday.addEventListener('click', moveWeek);
+    buttonToday.addEventListener('click', getToday);
     
     showWeek();
+    const date = new Date(date.getDate(+7) - date.getDate(-7));
+
     function moveWeek() {
-
-        const newWeek = [...arrWeeks];
-        newWeek.map(dateOfDay => {
-            const newDate = dateOfDay.getDate();
-    
-            dateOfDay = new Date(dateOfDay.setDate(newDate + 7));
-            dateOfDay = new Date(dateOfDay.setDate(newDate - 7));
-
-            showWeek(arrWeeks);
-
-        });
         
-
-        showWeek();
+       
+       
     };
+
+    function getToday() {
+    
+        const date = new Date();
+
+        return new Date(date.getDate());
+
+    }
 
 
 };

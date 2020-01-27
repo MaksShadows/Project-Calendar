9656c428109edc6b477d0fd452974f04a555c227
MaksShadows <Lykanenko1995@gmail.com>
2020-01-27 14:15:45 +0200

    add

--- src/script/switchingWeeks.js
+++ src/script/switchingWeeks.js
@@ -8,7 +8,7 @@ function switchWeeks() {
     const arowsRight = document.querySelector('.navigate__arows_right');
     arowsRight.addEventListener('click', moveWeek);
     const arowsLeft = document.querySelector('.navigate__arows_left');
-    arowsLeft.addEventListener('click', lastWeek);
+    arowsLeft.addEventListener('click', moveWeek);
     //const buttonToday = document.querySelector('.navigate_today');
     //buttonToday.addEventListener('click', getToday);
     
@@ -17,23 +17,25 @@ function switchWeeks() {
     function moveWeek() {
         let monday = new Date(getItem('monday'));
         let nextMon = new Date(new Date(monday).setDate(new Date(monday).getDate() + 7));
-
-        setItem('monday', nextMon);
-
-        let arr = getCurWeekDates(nextMon);
-
-        showWeek(arr);
-    };
-    function lastWeek() {
-        let monday = new Date(getItem('monday'));
         let lastMon = new Date(new Date(monday).setDate(new Date(monday).getDate() - 7));
+      
 
-        setItem('monday', lastMon);
+        setItem('monday', nextMon, lastMon);
 
-        let arr = getCurWeekDates(lastMon);
+        let arr = getCurWeekDates(nextMon, lastMon);
 
         showWeek(arr);
     };
+    // function lastWeek() {
+    //     let monday = new Date(getItem('monday'));
+    //     let lastMon = new Date(new Date(monday).setDate(new Date(monday).getDate() - 7));
+
+    //     setItem('monday', lastMon);
+
+    //     let arr = getCurWeekDates(lastMon);
+
+    //     showWeek(arr);
+    // };
 };
 
 

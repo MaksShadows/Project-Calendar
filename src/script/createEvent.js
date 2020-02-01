c9314cf9d6ce55c526b15fdaa334e85a1b4e3cbc
MaksShadows <Lykanenko1995@gmail.com>
2020-02-01 14:30:58 +0200

    add

--- src/script/createEvent.js
+++ src/script/createEvent.js
@@ -1,37 +1,24 @@
 import { setItem, getItem } from './storage.js';
 
 
-const formFields = {
-    name: document.querySelector('.event__name'),
-    dateStart: document.querySelector('.event__date-start'),
-    dateEnd: document.querySelector('.event__date-end'),
-    timeStart: document.querySelector('.event__time-start'),
-    timeEnd: document.querySelector('.event__time-end'),
-    description: document.querySelector('.event__description'),
-    color: document.querySelector('.event__color-picker'),
-};
-
-
 const btnSave = document.querySelector('.event__btn-save');
 
-function createEvent() {
-    btnSave.addEventListener('click', createObjectEvent);
-};
-
-function createObjectEvent(event) {
+    const  eventHandler = event => {
     event.preventDefault();
  
- 
 
-    let newEvent = {};
+    const events = getItem('events') || {};
+
 
     
 
-
-
     document.querySelector('.popup-alert').classList.toggle('display-none');
 };
-export { createEvent };
+btnSave.addEventListener('click', eventHandler);
+
+export { eventHandler };
+
+
 
 
 
--- src/script/index.js
+++ src/script/index.js
@@ -2,14 +2,14 @@ import { addContentSidebar } from './createSidebar.js';
 import { showWeek, getCurWeekDates, getMonday } from './showWeek.js';
 import { switchWeeks } from './switchingWeeks.js';
 import { showPopupWindow } from './showPopupWindow.js';
-import { createEvent } from './createEvent.js';
+import { eventHandler } from './createEvent.js';
 import { deleteObjectEvent } from './deleteEvent.js';
 
 addContentSidebar();
 showWeek(getCurWeekDates(getMonday()));
 switchWeeks();
 showPopupWindow();
-createEvent();
+eventHandler();
 deleteObjectEvent();
 
 //window.addEventListener('storage', );

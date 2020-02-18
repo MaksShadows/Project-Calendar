8277be2a69723a22d6787d526e082f020ab3ac69
MaksShadows <Lykanenko1995@gmail.com>
2020-02-18 11:53:07 +0200

    add

--- src/script/createEvent.js
+++ src/script/createEvent.js
@@ -1,16 +1,16 @@
 
-// const btnSave = document.querySelector('.event__btn-save');
+ const btnSave = document.querySelector('.event__btn-save');
 
-//  const  eventHandler = event => { 
-//      event.preventDefault();
-//      const popup = document.querySelector('.popup');
-//      const formData = [...new formData(popup)].reduce((acc, [field, value]) => ({ ...acc, [field]: value }), {})
-//     alert(JSON.stringify(formData));
-//    document.querySelector('.popup-alert').classList.toggle('display-none');
-// };
-// btnSave.addEventListener('click', eventHandler);
+  const  eventHandler = event => { 
+      event.preventDefault();
+      const popup = document.querySelector('.popup');
+      const formData = [...new formData(popup)].reduce((acc, [field, value]) => ({ ...acc, [field]: value }), {})
+     alert(JSON.stringify(formData));
+    document.querySelector('.popup-alert').classList.toggle('display-none');
+ };
+ btnSave.addEventListener('click', eventHandler);
 
-// export { eventHandler };
+ export { eventHandler };
 
 
 
--- src/script/redLine.js
+++ src/script/redLine.js
@@ -3,12 +3,13 @@ const redLine = document.querySelector(".redLine");
 function  renderRedLine() {
   const redlineElem = document.createElement('div');
    redlineElem.classList.add('redLine');
-
- 
-
    redLine.append(redLine);
 
+  
+  
+ 
 };
+
 
 
 export { renderRedLine };

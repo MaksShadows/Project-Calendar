50bae919eaee9eef6ad80d5ff9a890ff9ff7b648
MaksShadows <Lykanenko1995@gmail.com>
2020-03-26 21:21:04 +0200

    add

--- src/script/createTable.js
+++ src/script/createTable.js
@@ -1,4 +1,3 @@
-
 export const createNumbersArray = (from, to) => {
     const result = [];
 
@@ -8,15 +7,19 @@ export const createNumbersArray = (from, to) => {
 
     return result;
 };
-
+  
+export let today = new Date();
 
 const getDays = () => {
     const result = [];
     createNumbersArray(0, 6).map(day => {
+
+        const newDay = new Date(today);
+        newDay.setDate(newDay.getDate() + day);
         result.push(`
             <div
                 class="column-day"
-                data-day="${new Date().getDate()}"
+                data-day="${new Date(newDay).getDate()}"
             >${getItem()}</div>
         `);
     });
@@ -32,15 +35,28 @@ export const getItem = () =>
                 ></div>
             `).join('');
        
-            
+
+const getTime = () => createNumbersArray(1, 24)
+.map(timeItem => `
+    <div
+        class="sidebar hour"
+        data-line="${timeItem}"
+    >${ timeItem  <= 10 ?
+         `${timeItem}:00` :
+         `${timeItem}:00`}</div>
+    `).join('');
 
 const Column = document.querySelector('.calendar__week');
 export const addContentSidebar = () => {
     
-  
+    const time = getTime();
     const daysColumn = getDays();
     const timeSectors = createNumbersArray(1, 1)
         .map(lineTime => `
+            <div
+                class='sidebar hour'
+                data-column-number ="${lineTime}"
+            >${time}</div>
             <div
                 class='calendar__week'
                 data-column-number ="${lineTime}"

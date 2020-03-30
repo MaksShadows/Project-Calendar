459cef2cc0c8503d8b9d9f9f7411f8936b2010a7
MaksShadows <Lykanenko1995@gmail.com>
2020-03-30 14:09:25 +0300

    remnder

--- src/script/renderEvent.js
+++ src/script/renderEvent.js
@@ -1,28 +1,68 @@
+import { setItem, getItem } from './storage.js';
+import shmoment from './shmoment.js';
+
 
 function renderEvent(event) {
 
   //const newEvents = [];
 
-      event.map(({title, eventDescription, eventTimeStart, entTimeEnd}) => {
+    event.map(({title, eventDescription, eventTimeStart, entTimeEnd}) => {
 
      const eventNew = document.createElement('div');
      eventNew.classList.add('day-event');
      eventNew.setAttribute('data-id-event', `${id}`);
-     
+
      //eventNew = document.querySelector(`div[data-time = "${ }"]`);
 
-     const eventTime = document.createElement('div');
-     eventTime.append(`${new Date(eventTimeStart)} - ${new Date(entTimeEnd)}`);
+      const eventTime = document.createElement('div');
+      eventTime.append(`${new Date(eventTimeStart)} - ${new Date(entTimeEnd)}`);
 
-      eventNew.append(eventTime );
-     
+       eventNew.append(eventTime );
+
+    
      eventNew.innerHTML = `${title}`;
      eventNew.innerHTML = `${eventTime}`;
      eventNew.innerHTML = `${eventDescription}`;
 
   });
+  
+  event.filter(event => {
+    return event.start >= startEvent && event.end < endEvent;
+  });
+   event.forEach( event => {
+    const { start } = event;
+    const eventElem = createEventElement(event);
+
+    const slotElem = document.querySelector(
+     `.column-day[data-day="${start.getDate()}"] .column-day__item[data-time="${start.getHours()}"]`,
+        );
+        slotElem.append(eventElem);
+   });
+
 
 } ;
 
 
 export { renderEvent}
+
+
+// export const renderEvents = () => {
+//   removeEventsFromCalendar();
+//   const events = getItem('events') || [];
+//   const startDateTime = getItem('displayedWeekStart');
+//   const endDateTime = shmoment(startDateTime)
+//       .add('days', 7)
+//       .result();
+//   events
+//       .filter(event => {
+//           return event.start >= startDateTime && event.end < endDateTime;
+//       })
+//       .forEach(event => {
+//           const { start } = event;
+//           const eventElem = createEventElement(event);
+//           const slotElem = document.querySelector(
+//               `.column-day[data-day="${start.getDate()}"] .column-day__item[data-time="${start.getHours()}"]`,
+//           );
+//           slotElem.append(eventElem);
+//       });
+// };

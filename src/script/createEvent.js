1c2ed248874e2962c50171ff1692cafd544a717c
MaksShadows <Lykanenko1995@gmail.com>
2020-03-30 10:44:14 +0300

    render

--- src/script/createEvent.js
+++ src/script/createEvent.js
@@ -4,6 +4,15 @@ const eventHandler = (event) => {
      event.preventDefault();
      const popup = document.querySelector('.popup');
      const formData = [...new FormData(popup)].reduce((acc, [field, value]) => ({ ...acc, [field]: value }), {})
+
+     const eventTitle = document.querySelector('.event__name');
+     const eventDate = document.querySelector('.event__date-start');
+     const eventstartTime = document.querySelector('.event__time-start');
+     const eventendTime = document.querySelector('.event__time-end ');
+     const eventDescription = document.querySelector('.event__description');
+
+
+
      alert(JSON.stringify(formData));
      document.querySelector('.popup-alert').classList.toggle('display-none');
    };
--- src/script/renderEvent.js
+++ src/script/renderEvent.js
@@ -1,14 +1,28 @@
 
+function renderEvent(event) {
 
-function renderEvent() {
+  //const newEvents = [];
 
-   //const newEvents = [];
+      event.map(({title, eventDescription, eventTimeStart, entTimeEnd}) => {
 
-   //const event = document.createElement('div');
-   // event.classList.add('day-event');
-   
- // document.querySelector('.day-event').setAttribute('data-id-event', event.id);
+     const eventNew = document.createElement('div');
+     eventNew.classList.add('day-event');
+     eventNew.setAttribute('data-id-event', `${id}`);
+     
+     //eventNew = document.querySelector(`div[data-time = "${ }"]`);
 
+
+     const eventTime = document.createElement('div');
+     eventTime.append(`${new Date(eventTimeStart)} - ${new Date(entTimeEnd)}`);
+
+      eventNew.append(eventTime );
+     
+     eventNew.innerHTML = `${title}`;
+     eventNew.innerHTML = `${eventTime}`;
+     eventNew.innerHTML = `${eventDescription}`;
+
+
+  });
 
 } ;
 
